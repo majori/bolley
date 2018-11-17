7e31a8885b2b5bfe7ee6f905359efc0803e6a991
Antti Kivimäki <antti.kivimaki@futurice.com>
2018-11-17 16:35:29 +0200

    Authorize "/upload" route

--- src/http.go
+++ src/http.go
@@ -9,9 +9,46 @@ import (
 )
 
 func listen() {
-	r := gin.Default()
-	r.POST("/upload", upload)
-	r.Run("0.0.0.0:" + os.Getenv("PORT")) // listen and serve on 0.0.0.0:8080
+	r := gin.New()
+
+	// Public
+	public := r.Group("/")
+	public.GET("/ping", pong)
+
+	// Private
+	private := r.Group("/")
+	private.Use(TokenAuthMiddleware())
+	private.POST("/upload", upload)
+	r.Run(":" + os.Getenv("PORT")) // listen and serve on 0.0.0.0:8080
+}
+
+func respondWithError(code int, message string, c *gin.Context) {
+	resp := map[string]string{"error": message}
+
+	c.JSON(code, resp)
+	c.Abort()
+}
+
+func TokenAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.Request.FormValue("api_token")
+
+		if token == "" {
+			respondWithError(401, "API token required", c)
+			return
+		}
+
+		if token != os.Getenv("API_TOKEN") {
+			respondWithError(401, "Invalid API token", c)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func pong(c *gin.Context) {
+	c.String(200, "pong")
 }
 
 // upload logic
